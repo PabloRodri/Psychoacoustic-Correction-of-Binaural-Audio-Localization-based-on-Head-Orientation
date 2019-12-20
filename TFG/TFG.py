b304b940621616ac8acdf591f1b6d621b91a04eb
Pablo Rodríguez Cuenca <pablo.rodriguez01@estudiant.upf.edu>
2019-12-20 12:28:48 +0100

    DFT+DirEst_Restart_20/12/19

--- TFG/TFG.py
+++ TFG/TFG.py
@@ -4,10 +4,13 @@ import matplotlib.pyplot as plt
 import scipy.signal
 import soundfile as sf
 import time as tttt
+from scipy.stats import circmean
 from pysofaconventions import *
 from cmath import e, pi, sin, cos
 np.seterr(divide='ignore', invalid='ignore') #To avoid "divide by zero" or "divide by NaN" errors
 
+t1 = tttt.time()  
+print('Starting...')
 
 #FILE
 #Set audio files directory
@@ -24,64 +27,34 @@ audios = sofa.getDataIR() # Read the data and create data array (8802, 2, 256)
 sfreq = int(sofa.getSamplingRate()) #samplingRateUnits = sofa.getSamplingRateUnits() if I want the units of the sampling frequency
 numfiles = MU_cb = len(audios)
 numchannels = len(audios[0])
-numsamples = PHI = len(audios[0][0])
+numsamples = len(audios[0][0])
 
 
 
-#PEAKS ARRAYS and CODEBOOK CREATION
-time = np.zeros((numfiles, 256))
-peaks = np.zeros((numfiles, 2))
-tpeaks = np.zeros((numfiles, 2))
-codebook_f = np.zeros((numfiles, numsamples))
+#CODEBOOK CREATION
 codebook = np.zeros((numfiles, numchannels, numsamples),dtype=complex)
 for x in range(numfiles): 
-    #3D array for audios + time array
-    time[x] = np.arange(0,int(len(audios[x,0]))) / sfreq #Time array, ordered by audiofile
-    #Previous ILD & ITD work (from input audio)
-    peaks[x] = np.amax(audios[x], axis=1) #Peak array, ordered by audiofile
-    extractionL, = np.where(audios[x,0]==peaks[x,0])
-    extractionR, = np.where(audios[x,1]==peaks[x,1])
-    tpeaks[x] = [max(extractionL),max(extractionR)] #Time position array, ordered by audiofile
-
     #DFT sample frequencies, ordered by audiofile
-    codebook_f[x] = np.fft.fftfreq(audios[x,0].shape[-1]) #L and R share the same values
+    #codebook_f[x] = np.fft.fftfreq(audios[x,0].shape[-1]) #L and R share the same values
+
     #Codebook of HRTFs, ordered by audiofile
     codebook[x,0] = np.fft.fft(audios[x,0])
     codebook[x,1] = np.fft.fft(audios[x,1])
 
 
-'''
-#ILD and IPD CALCULATION
+#RULE OUT ELEVATION
 #Taking only samples with zero elevation
 ind_azim = []
-ILD = []
-ITD = []
-IPD = []
 azimuth = []
-idealazimuth = []
 for x in range(numfiles):
     if (sP[:][x][1] == 0) == True:
         ind_azim.append(x)
-ILDc = np.zeros((360, int(numsamples/2))) #Only of the first half of samples as it's symmetrical
-IPDc = np.zeros((360, int(numsamples/2))) #Only of the first half of samples as it's symmetrical
-x=0
 for a in ind_azim:
-    azimuth.append(float(sP[a][0])) #Azimuth array [0,360)
     sPaz = float(sP[a][0])
-    if sPaz > 180: #Quadrant correction
-        sPaz = int(-(sPaz-180))
-    idealazimuth.append(sPaz) #Azimuth array [-180,180)
-    
-    if (azimuth[x]).is_integer() == True:
-        ILD.append(-20 * mt.log10(abs(peaks[a,0]/peaks[a,1]))) #Peak level difference (in dB). Negative to make the negative side of the grids the right ear as in https://www.york.ac.uk/sadie-project/database.html
-        ITD.append(10**(-4)*(tpeaks[a,0]-tpeaks[a,1])) #Time difference between peaks (*10^-4). 
-        IPD.append(-20 * mt.log10(abs(peaks[a,0]/peaks[a,1]))*2*np.pi*sfreq) #IPD = ILD*2πf
+    #if sPaz > 180: #Quadrant correction
+        #sPaz = int(-(sPaz-180))
+    azimuth.append(sPaz) #Create array with the ideal angles for the plot
 
-    for b in range(int(numsamples/2)):
-        ILDc[int(idealazimuth[x]), b] = -10 * mt.log10(np.abs(codebook[a,1,b])/np.abs(codebook[a,0,b])) #ILD (following eq 6 from the paper) (in dB)
-        IPDc[int(idealazimuth[x]), b] = np.angle((codebook[a,1,b]/codebook[a,0,b]), deg=False) #IPD in radians (following eq 7 from the paper)
-    x+=1
-'''
 
 
 #RENDER WITH A FILE TO CREATE BINAURAL AUDIO
@@ -94,72 +67,68 @@ binaural = np.asarray([binaural_L, binaural_R]) #.swapaxes(-1,0) to put the L/R 
 #sf.write('C:/Users/pablo/Desktop/resultadobinaural.wav', binaural.swapaxes(-1,0), samplerate) #Save into a WAV file
 
 #DFT WINDOWING
-BINAURAL_L = scipy.signal.stft(binaural_L, samplerate) #STFT of the binaural input to convert it to the time-freq domain    #nperseg=256 by default
-BINAURAL_R = scipy.signal.stft(binaural_R, samplerate) #STFT of the binaural input to convert it to the time-freq domain 
+BINAURAL_L = scipy.signal.stft(binaural_L, samplerate/2) #STFT of the binaural input to convert it to the time-freq domain    #nperseg=256 by default       #Samplingrate/2 to decrease processing time
+BINAURAL_R = scipy.signal.stft(binaural_R, samplerate/2) #STFT of the binaural input to convert it to the time-freq domain                                  #Samplingrate/2 to decrease processing time
 BINAURAL_LR = np.asarray([binaural_L, binaural_R])
 MU, LAMBDA, Sxx = scipy.signal.spectrogram(BINAURAL_LR, samplerate, mode='complex')
+PHI = np.arange(360) #360 gradians
 BINAURAL = np.swapaxes(Sxx, 1, 2)
 #sf.write('C:/Users/pablo/Desktop/resultadobinauralpostwindowing.wav', BINAURAL.swapaxes(-1,0), samplerate) #Save into a WAV file
-'''
-plt.pcolormesh(LAMBDA, MU, 10*np.log10(abs(Sxx[0])))
-plt.xlabel('Time (λ)')
-plt.ylabel('Frequency (μ)')
-plt.show()
-'''
-t1 = tttt.time()
 
+t2 = tttt.time()  
+print('Codebook ILDs and IPDs calculation...   Elapsed time = ' + str(t2-t1))
+#Codebook ILDs and IPDs
+ILDh = np.zeros((len(MU), len(PHI)))
+IPDh = np.zeros((len(MU), len(PHI)))
+for lamb in range(5): #Time index                       #len(LAMBDA)
+    for mu in range(len(MU)): #Frequency index
+        #Codebook differences (eqs.6-7)
+        for x in range(len(ind_azim)): #ind_azim = [12, 35, 57, 79, 101, 123, 145, 167, 189, 211, 233, 255, 277, 310, ...]     azimuth = [0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, ...]
+            ILDh[mu, int(azimuth[x])] = np.abs(codebook[ind_azim[x],1,mu])/np.abs(codebook[ind_azim[x],0,mu])
+            IPDh[mu, int(azimuth[x])] = np.angle(codebook[ind_azim[x],1,mu]) - np.angle(codebook[ind_azim[x],0,mu]) #We use the second part of the eq as it's in rad and not in grad as the other one
+
+t3 = tttt.time()  
+print('Input file ILDs and IPDs + Phi_origin calculation...   Elapsed time = ' + str(t3-t1))
+#Input file ILDs and IPDs + Phi_origin
 ILD = np.zeros((len(LAMBDA), len(MU)))
 IPD = np.zeros((len(LAMBDA), len(MU)))
-ILDh = np.zeros((len(MU), PHI))
-IPDh = np.zeros((len(MU), PHI))
-F = np.zeros((PHI))
+F = np.zeros((len(PHI)))
 phi_orig = np.zeros((len(LAMBDA), len(MU)))
-#FOR IMPROVED
-for lamb in range(len(LAMBDA)): #Time index
+for lamb in range(5): #Time index                       #len(LAMBDA)
     for mu in range(len(MU)): #Frequency index
         #Input signal differences (eqs.4-5)
         ILD[lamb, mu] = np.abs(BINAURAL[1,lamb,mu]/BINAURAL[0,lamb,mu])
         IPD[lamb, mu] = np.angle(BINAURAL[1,lamb,mu]) - np.angle(BINAURAL[0,lamb,mu]) #float to prevent floor-division -> floor(int)/0   #We use the second part of the eq as it's in rad and not in grad as the other one
-        #Codebook differences (eqs.6-7)
-        phi = 0
-        ILDh[mu] = [np.abs(codebook[phi,1,mu])/np.abs(codebook[phi,0,mu]) for phi in range(PHI)]
-        IPDh[mu] = [np.angle(codebook[phi,1,mu]) - np.angle(codebook[phi,0,mu]) for phi in range(PHI)] #We use the second part of the eq as it's in rad and not in grad as the other one
-        F = [np.angle(ILDh[mu,phi]/ILD[lamb, mu]) + (ILD[lamb, mu]/ILDh[mu,phi]) - 2*np.cos(IPD[lamb, mu] - IPDh[mu,phi]) for phi in range(PHI)] #Azimuth estimation for every TF bin (output needs to be a complex number)
-        
-        phi_orig[lamb, mu] = np.min(F) #phi which gives the minimum F
+        F = [np.angle(ILDh[mu,phi]/ILD[lamb, mu]) + (ILD[lamb, mu]/ILDh[mu, phi]) - 2*np.cos(IPD[lamb, mu] - IPDh[mu, phi]) for phi in PHI] #Azimuth estimation for every TF bin (output needs to be a complex number
+        phi_orig[lamb, mu] = np.argmin(F) #phi index which gives the minimum F
+                            #phi_orig[lamb, mu] = np.unravel_index(np.argmin(F, axis=None), F.shape)
+                            #F[phi_orig[lamb, mu]]
         #print('lambda:' + str(lamb) + ' mu:' + str(mu))
-''' #FOR NORMAL
-for lamb in range(15): #Time index
-    for mu in range(len(MU)): #Frequency index
-        #Input signal differences (eqs.4-5)
-        ILD[lamb, mu] = np.abs(BINAURAL[1,lamb,mu]/BINAURAL[0,lamb,mu])
-        IPD[lamb, mu] = np.angle(BINAURAL[1,lamb,mu]) - np.angle(BINAURAL[0,lamb,mu]) #float to prevent floor-division -> floor(int)/0   #We use the second part of the eq as it's in rad and not in grad as the other one
-        #Codebook differences (eqs.6-7)
-        for phi in range(PHI): #Frequency index
-            ILDh[mu] = np.abs(codebook[phi,1,mu])/np.abs(codebook[phi,0,mu])
-            IPDh[mu] = np.angle(codebook[phi,1,mu]) - np.angle(codebook[phi,0,mu]) #We use the second part of the eq as it's in rad and not in grad as the other one
-            F[phi] = np.angle(ILDh[mu,phi]/ILD[lamb, mu]) + (ILD[lamb, mu]/ILDh[mu,phi]) - 2*np.cos(IPD[lamb, mu] - IPDh[mu,phi]) #Azimuth estimation for every TF bin (output needs to be a complex number)
-        
-        phi_orig[lamb, mu] = np.min(F) #phi which gives the minimum F
-        #print('lambda:' + str(lamb) + ' mu:' + str(mu))
-'''
-t2 = tttt.time()    
 
-print(t2-t1)
+avg_angle = np.rad2deg( circmean(np.deg2rad(phi_orig[:5])) )                    #phi_orig[:5]
+print('Origin phi is: ' + str(avg_angle))
 
-#for improved: 59.6130530834198
-#for improved sin prints: 50.38822388648987
-#for: 59.65979218482971
-#for, fixed abs: 56.1263484954834
-#for, fixed abs & no prints: 48.043715476989746
-#for improved, fixed abs & no prints: 40.789695024490356
+t4 = tttt.time()    
+print('Total time (plots not included): ' + str(t4-t1) + ' seconds')
 
-#Total time: 17,784864552815755‬ minutes
-#Total time2: 13,680182230472565‬ minutes
-#Total time 3 (if using laptop): 25,6325592 minutes
+#Full 1 - Input file ILDs and IPDs + Phi_origin calculation...   Elapsed time = 1673.2812156677246                          27.888020261128744 minutes
+#Full 1 - Total time (plots not included): 2368.8777115345 seconds                                                          39.48129519224167 minutes
+#Full 2 - Input file ILDs and IPDs + Phi_origin calculation...   Elapsed time = 897.7831192016602                           14.963051986694336666666666666667 minutes
+#Full 2 - Total time (plots not included): 1302.7927815914154 seconds                                                       21.71321302652359‬ minute (laptop just started and without using it)
+#Full 3(STFT samplerate/2) - Input file ILDs and IPDs + Phi_origin calculation...   Elapsed time = 868,56902384758          14,476150397459666666666666666667‬ minutes !!
+#Full 3(STFT samplerate/2) - Total time (plots not included): 1298,391851902008 seconds                                     21,6398641983668‬ minutes !!
+
+#Partial 1 - Input file ILDs and IPDs + Phi_origin calculation...   Elapsed time = 18.076984167099
+#Partial 1 - Total time (plots not included): 24.700349807739258 seconds
+#Partial 2(STFT samplerate/2) - Input file ILDs and IPDs + Phi_origin calculation...   Elapsed time = 16.646246671676636
+#Partial 2(STFT samplerate/2) - Total time (plots not included): 23.171047687530518 seconds
+#Partial 3(STFT samplerate/4) - Input file ILDs and IPDs + Phi_origin calculation...   Elapsed time = 21.892667293548584
+#Partial 3(STFT samplerate/4) - Total time (plots not included): 29.640634775161743 seconds
+#Partial 4(STFT samplerate/2) - Input file ILDs and IPDs + Phi_origin calculation...   Elapsed time = 11.035381317138672!!
+#Partial 4(STFT samplerate/2) - Total time (plots not included): 15.686950922012329 seconds!!
 
 #PLOTS
-plt.pcolormesh(LAMBDA, MU, np.swapaxes(phi_orig, 0, 1), cmap=plt.cm.get_cmap('hsv')) #Inverted axis to have the plot we want
+plt.pcolormesh(LAMBDA, MU, np.swapaxes(phi_orig, 0, 1)) #Inverted axis to have the plot we want
 plt.title('pcolormesh KU100')
 plt.xlabel('Time (λ)')
 plt.ylabel('Frequency (μ)')
