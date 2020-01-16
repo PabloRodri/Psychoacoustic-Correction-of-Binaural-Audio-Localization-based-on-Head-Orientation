ab6e85322ed2e3a4d896e7e8cd63b60c88211a3f
Pablo Rodríguez Cuenca <pablo.rodriguez01@estudiant.upf.edu>
2020-01-16 18:45:14 +0100

    CueModification_16/01/20

--- TFG/TFG.py
+++ TFG/TFG.py
@@ -9,16 +9,41 @@ from pysofaconventions import *
 from cmath import e, pi, sin, cos
 #np.seterr(divide='ignore', invalid='ignore') #To avoid "divide by zero" or "divide by NaN" errors
 
+def circmedian(angs, unit='rad'):
+    # from invalid index to scalar variable.
+    # Radians!
+    pdists = angs[np.newaxis, :2000] - angs[:2000, np.newaxis] #Discard of higher time and frequency values to improve performance and results (as there are a lot of outliers in those zones)
+    if unit == 'rad':
+        pdists = (pdists + np.pi) % (2 * np.pi) - np.pi
+    elif unit == 'deg':
+        pdists = (pdists +180) % (360.) - 180.
+    pdists = np.abs(pdists).sum(1)
+
+    # If angs is odd, take the center value
+    if len(angs) % 2 != 0:
+        return angs[np.argmin(pdists)]
+    # If even, take the mean between the two minimum values
+    else:
+        index_of_min = np.argmin(pdists)
+        min1 = angs[index_of_min]
+        # Remove minimum element from array and recompute
+        new_pdists = np.delete(pdists, index_of_min)
+        new_angs = np.delete(angs, index_of_min)
+        min2 = new_angs[np.argmin(new_pdists)]
+        if unit == 'rad':
+            return scipy.stats.circmean([min1, min2], high=np.pi, low=-np.pi)
+        elif unit == 'deg':
+            return scipy.stats.circmean([min1, min2], high=180., low=-180.)
+
 t1 = tttt.time()  
 print('Starting...\n ---------------')
-
+##########################################################################################################
 #FILE
 #Set audio files directory
 #sofa = SOFAFile(r'C:\Users\pablo\source\repos\TFG\Muestras\KU100_44K_16bit_256tap_FIR_SOFA.sofa','r')
 #sofa = SOFAFile(r'C:\Users\pablo\source\repos\TFG\Muestras\Otras\QU_KEMAR_anechoic.sofa','r') 
 sofa = SOFAFile(r'C:\Users\pablo\source\repos\TFG\Muestras\Otras\QU_KEMAR_anechoic_1m.sofa','r')
 #sofa = SOFAFile(r'C:\Users\pablo\source\repos\TFG\Muestras\Otras\NFHRIR_CIRC360_SOFA\HRIR_CIRC360_NF100.sofa','r')
-
 if sofa.isValid(): #Check validity
     print("SOFA file is valid")
 else:
@@ -36,11 +61,9 @@ angles = sourcePositions[:,0]
 #samplingRateUnits = sofa.getSamplingRateUnits() if I want the units of the sampling frequency
 #receiverPos = sofa.getReceiverPositionValues() to get the receiver ears position
 '''
-
 audios = sofa.getDataIR() # Read the data and create data array (8802, 2, 256)
 numangles, numchannels, numsamples = audios.shape
 PHI = numangles #Number of files
-
 
 
 #CODEBOOK CREATION of HRTFs, ordered by audiofile
@@ -52,7 +75,6 @@ codebook[:,1] = np.fft.fft(audios[:,1])
 #DFT sample frequencies, ordered by audiofile
 #codebook_f[:] = np.fft.fftfreq(audios[:,0].shape[-1]) #L and R share the same values
 '''
-
 
 ''' #RULE OUT ELEVATION
 #Taking only samples with zero elevation
@@ -85,8 +107,6 @@ for a in ind_azim:
     azimuth.append(np.round(sPaz)) #Create array with the ideal angles for the plot
 '''
 
-
-
 #RENDER WITH A FILE TO CREATE BINAURAL AUDIO
 #data, samplerate = sf.read(r'C:\Users\pablo\source\repos\TFG\Muestras\Otras\audiocheck.net_whitenoisegaussian.wav') #Open a mono wav gaussian noise.
 data, samplerate = sf.read(r'C:\Users\pablo\source\repos\TFG\Muestras\LNG_VocalLaugh_25.wav') #Open a mono wav file. I got this one from freesound https://freesound.org/people/Ryntjie/sounds/365061/ 
@@ -115,7 +135,8 @@ MU, LAMBDA, Y = scipy.signal.stft(binaural, sfreq, nperseg = numsamples) #Y to s
 t2 = tttt.time()  
 print('Elapsed time = ' + str(t2-t1) + '\n ---------------')
 print('Codebook ILDs and IPDs calculation...')
-
+##########################################################################################################
+#DIRECTION ESTIMATION
 #Codebook ILDs and IPDs
 '''#Old ILDh & IPDh Loop
 ILDh = np.zeros((len(MU), len(PHI)))
@@ -130,11 +151,9 @@ ILDcb = np.transpose(np.abs(codebook[:,1,:])/np.abs(codebook[:,0,:])) # ILDcb ha
 IPDcb = np.transpose(np.angle(codebook[:,1,:]) - np.angle(codebook[:,0,:])) # IPDcb has shape len(MU), numangles
 IPDcb = IPDcb % (2*np.pi) #Normalization between (0,2π)
 
-
 t3 = tttt.time()  
 print('Elapsed time = ' + str(t3-t1) + '\n ---------------')
-print('Input file ILDs and IPDs + Phi_origin calculation...')
-
+print('Input file ILDs and IPDs...')
 #Input file ILDs and IPDs + direction estimation
 '''#Old ILD & IPD Loop
 ILD = np.zeros((len(LAMBDA), len(MU)))
@@ -151,30 +170,101 @@ for lamb in range(len(LAMBDA)): #Time index                       #len(LAMBDA)
         phi_orig[lamb, mu] = np.argmin(F.tolist()) #phi index which gives the minimum F
 '''
 F = np.zeros((PHI))
-phi_orig = np.zeros((len(LAMBDA), len(MU)))
+phi_orig = np.zeros((len(LAMBDA), len(MU)), dtype=int)
 ILDy = np.abs(Y[1, :,:])/np.abs(Y[0, :, :])
 IPDy = np.angle(Y[1, :,:]) - np.angle(Y[0, :, :])
 for lamb in range(len(LAMBDA)): #Time index                 #len(LAMBDA)
     for mu in range(len(MU)): #Frequency index
         F[:] = ILDcb[mu, :] / ILDy[mu, lamb] + ILDy[mu, lamb] / ILDcb[mu, :] - 2*np.cos( IPDy[mu, lamb] - IPDcb[mu, :])
-        phi_orig[lamb, mu] = np.argmin(F) #phi index which gives the minimum F
+        phi_orig[lamb, mu] = sourcePositions[int(np.argmin(F)), 0] #phi index which gives the minimum F
 
-#avg_angle = np.rad2deg( circmean(np.deg2rad(phi_orig[:])) )
-avg_angle = np.rad2deg(  circmean(np.deg2rad(sourcePositions[ int(np.median(phi_orig[:])) ,0] ))  )
-print('Estimated direction: ' + str(avg_angle) + 'º')
 
 t4 = tttt.time()    
-print('Total time (plots not included): ' + str(t4-t1) + ' seconds')
+print('Elapsed time = ' + str(t4-t1) + '\n ---------------')
+print('Phi origin calculation...')
+#PHI ORIGIN CALCULATION
+ta1 = tttt.time()    
+angle_mean = round(np.rad2deg( circmean(np.deg2rad(phi_orig[:])) ),1) #Rounded to 1 decimal
+ta2 = tttt.time()
+print('Estimated direction by mean: ' + str(angle_mean) + 'º' + ' in ' + str(ta2-ta1) + ' seconds')
 
+ta3 = tttt.time()
+p_o = np.round(phi_orig.flatten(),1) #2D array into 1D (with 1 decimal)
+angle_median = float(circmedian(p_o))
+ta4 = tttt.time()
+print('Estimated direction by median: ' + str(angle_median) + 'º' + ' in ' + str(ta4-ta3) + ' seconds')
 
-#PLOTS
+ta5 = tttt.time()    
+hist, _, _ = plt.hist(p_o, bins=360)
+angle_hist = float(np.argmax(hist))
+ta6 = tttt.time()
+#plt.title("Phi_orig histogram")
+#plt.show()
+print('Estimated direction by histogram: ' + str(angle_hist) + 'º' + ' in ' + str(ta6-ta5) + ' seconds')
+
+'''#PLOTS
 plt.pcolormesh(LAMBDA, MU, np.swapaxes(phi_orig, 0, 1)) #Inverted axis to have the plot we want
 plt.title('pcolormesh KU100 indexes')
 plt.xlabel('Time (λ)')
 plt.ylabel('Frequency (μ)')
 plt.colorbar(aspect=6)
 plt.show()
+'''
 
+
+t5 = tttt.time()  
+print('Elapsed time = ' + str(t5-t1) + '\n ---------------')
+print('Cue modification with headtracker...')
+##########################################################################################################
+#CUE MODIFICATION
+HTphi = np.zeros((len(LAMBDA), len(MU)))
+phi_dest = np.zeros((len(LAMBDA), len(MU)), dtype=int)
+
+HTphi[:,:] = 20 #Fake headtracker input angle
+phi_dest[:] = phi_orig[:] - HTphi[:]
+
+deltaIPD = np.zeros((len(LAMBDA), len(MU)), dtype=complex)
+G_IPDr = np.zeros((len(LAMBDA), len(MU)), dtype=complex)
+G_IPDl = np.zeros((len(LAMBDA), len(MU)), dtype=complex)
+G_ILDr = np.zeros((len(LAMBDA), len(MU)), dtype=complex)
+G_ILDl = np.zeros((len(LAMBDA), len(MU)), dtype=complex)
+for m in range(len(MU)): #Frequency index
+    deltaIPD[:,m] = IPDcb[m,phi_dest[:,m]] - IPDcb[m,phi_orig[:,m]]                                              #"IPDcb = IPDcb * (2*np.pi)" to desnormalize?
+
+    G_IPDr[:,m] = np.angle( -(deltaIPD[:,m]/2) )
+    G_IPDl[:,m] = np.angle(   deltaIPD[:,m]/2  )
+
+    G_ILDr[:,m] = np.transpose(np.abs(codebook[phi_dest[:,m],1,m])/np.abs(codebook[phi_orig[:,m],1,m]))             #To transpose or not to transpose????
+    G_ILDl[:,m] = np.transpose(np.abs(codebook[phi_dest[:,m],0,m])/np.abs(codebook[phi_orig[:,m],0,m]))             #To transpose or not to transpose????
+
+Gr = G_ILDr * G_IPDr
+Gl = G_ILDl * G_IPDl
+
+G = np.asarray([np.transpose(Gl), np.transpose(Gr)]) #Transpose to mantain the original variable format
+#iG_t, iG = scipy.signal.istft(G, sfreq, nperseg = numsamples) #stft to Y domain convertion with the same window size used above
+
+#delta = np.ones((numchannels, numsamples))
+G2 = np.swapaxes(np.swapaxes(G,1,2),0,1) #convert G (2, 1025, 99) to G2 (99, 2, 1025)
+iG = np.zeros((len(LAMBDA), numchannels, numsamples),dtype=complex)
+iG[:] = np.fft.irfft(G2[:])
+
+output_L = scipy.signal.fftconvolve(data,iG[0]) #Convolve it with the input audio 
+output_R = scipy.signal.fftconvolve(data,iG[1]) #Convolve it with the input audio 
+output = np.asarray([output_L, output_R]) #.swapaxes(-1,0) to put the L/R channel first
+sf.write('C:/Users/pablo/Desktop/resultadobinaural1.wav', output.swapaxes(-1,0), samplerate) #Save into a WAV file
+
+output_L2 = scipy.signal.fftconvolve(data,iG[:,0].flatten()) #Convolve it with the input audio 
+output_R2 = scipy.signal.fftconvolve(data,iG[:,1].flatten()) #Convolve it with the input audio
+output2 = np.asarray([output_L2, output_R2]) #.swapaxes(-1,0) to put the L/R channel first
+sf.write('C:/Users/pablo/Desktop/resultadobinaural2.wav', abs(output2).swapaxes(-1,0), samplerate) #Save into a WAV file
+
+OUTPUT = np.zeros((numangles, numchannels, numsamples),dtype=complex)
+OUTPUT[:] = np.fft.irfft(output[:])
+sf.write('C:/Users/pablo/Desktop/resultadobinaural3.wav', OUTPUT.swapaxes(-1,0), samplerate) #Save into a WAV file
+
+
+t999 = tttt.time()    
+print('Total time: ' + str(t999-t1) + ' seconds')
 print('This is the end')
 
 
